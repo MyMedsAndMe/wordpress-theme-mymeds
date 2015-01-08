7a28e488baf0c240c62f3cce6c88e8fa728833dc
John Wright <johngeorge.wright@gmail.com>
2015-01-08 15:16:54 +0000

    Category listings

--- category.php
+++ category.php
@@ -12,12 +12,12 @@
 get_header(); ?>
 
 	<section id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+		<div id="content" class="site-content category-listing" role="main">
 
 			<?php if ( have_posts() ) : ?>
 
 			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'mymeds' ), single_cat_title( '', false ) ); ?></h1>
+				<h1 class="archive-title"><?php printf( __( '%s', 'mymeds' ), single_cat_title( '', false ) ); ?></h1>
 
 				<?php
 					// Show an optional term description.
